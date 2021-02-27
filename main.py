073579a3131f39dc0c2c2f9ffb708049997e866f
Daniel Angel <dka190000@utdallas.edu>
2021-02-27 16:58:31 -0600

    bot's first steps wooooo

---
+++ main.py
@@ -0,0 +1,23 @@
+import discord
+
+client = discord.Client()
+
+f = open("key.txt", "r")
+key = f.read()
+f.close()
+
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run(key)
