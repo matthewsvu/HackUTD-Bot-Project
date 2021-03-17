6cdd77506e62511d00908e3b5a92486a53714aba
matthewsvu <msv180000@utdallas.edu>
2021-03-17 04:58:27 -0700

    added tags full functionality

--- scripts/rmp.py
+++ scripts/rmp.py
@@ -59,45 +59,46 @@ async def get_rating(message):
         embed.add_field(name="Would Take Again",
                     value=take_again, inline=False)
 
-    await message.channel.send(embed=embed)
+        await message.channel.send(embed=embed)
     except (RuntimeError, IndexError, AttributeError):
         await prof_not_found(message)
 
 async def get_tags(message):
-    arr = message.content.strip().split()
-    first_name = arr[1]
-    last_name = arr[2]
-    professor_name = first_name.title() + " " + last_name.title()
-    professor_name.replace(' ', '+')
+    try:
+        arr = message.content.strip().split()
+        first_name = arr[1]
+        last_name = arr[2]
+        professor_name = first_name.title() + " " + last_name.title()
 
-    # get UTD School object from API
-    school_name = rate.get_school_by_name(school)
-
-    # get the url of the professor's RMP page
-    url = "https://www.ratemyprofessors.com/search.jsp?queryoption=HEADER&queryBy=teacherName" \
-          "&schoolName=%s&schoolID=%s&query=%s" % (school_name.name, school_name.id, professor_name)
-    page = requests.get(url)
- 
-    # parse the html elements for the professor's tags
-    soup = bs(page.text, "html.parser")
-    prof_tags = soup.findAll("span", {"class": "TeacherTags_TagsContainer-sc-16vmh1y-0 dbxJaW" })
+        # get Professor object from API
+        professor = rate.get_professor_by_school_and_name(
+                rate.get_school_by_name(school),
+                professor_name)
+        
+        # get the url of the professor's RMP page
+        url = "https://www.ratemyprofessors.com/ShowRatings.jsp?tid=%s" % professor.id
+        page = requests.get(url)
+        
+        # parse the html elements for the professor's tags
+        soup = bs(page.text, "html.parser")
+        prof_tags = soup.find('div', 'TeacherTags__TagsContainer-sc-16vmh1y-0 dbxJaW').findAll("span", {"Tag-bs9vf4-0 hHOVKF"}, limit=5)
     
-    if(len(prof_tags) == 0):
-        await message.channel.send("Professor's tags could not be found.")
-        return
-    
-    # output the tags in a list
-    output = ''
-    for tag in prof_tags:
-        output += tag.get_text() + '\n'
-    
-    await message.channel.send(output)
+        if(len(prof_tags) == 0):
+            await message.channel.send("Professor's tags could not be found.")
+            return
+        
+        # output the tags in a formatted manner
+        output = ''.join(tag.get_text() + "\n" if i != len(prof_tags)-1 else tag.get_text() for i, tag in enumerate(prof_tags))
+        output_format = f"```{professor_name}'s top tags are:\n{output}```"
+        await message.channel.send(output_format)
+    except(RuntimeError, IndexError, AttributeError):
+        await prof_not_found(message)
 
 async def prof_not_found(message):
     emoji = u"\U0001F50E"
     embed = discord.Embed(
         title=f"{emoji} Ratings not found",
-        description="The professor's ratings could not be found.",
+        description="The professor's RateMyProfessor page could not be found.",
         color=0xC75B12
     )
     await message.channel.send(embed=embed)
