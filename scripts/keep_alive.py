d92c227216dc2d90161e3fb8f55859525bacfe7e
matthewsvu <msv180000@utdallas.edu>
2021-03-18 06:00:52 -0700

    FINALLY FIXED

--- scripts/keep_alive.py
+++ scripts/keep_alive.py
@@ -7,11 +7,10 @@ app = Flask('')
 
 @app.route('/')
 def home():
-    return render_template('../index.html')
+    return render_template('index.html')
 
 def run():
     app.run(host='0.0.0.0', port=8080)
-
 
 def keep_alive():
     t = Thread(target=run)
