5958eebeb424f0806ca3288af3fc838c2cf50d60
matthewsvu <msv180000@utdallas.edu>
2021-03-18 06:08:20 -0700

    pls work

--- scripts/keep_alive.py
+++ scripts/keep_alive.py
@@ -10,7 +10,7 @@ def home():
     return render_template('index.html')
 
 def run():
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0')
 
 def keep_alive():
     t = Thread(target=run)
