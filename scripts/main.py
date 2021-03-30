b02f2cfa4c67fd8785ce6f7a82f658a59cdd295e
matthewsvu <msv180000@utdallas.edu>
2021-03-30 03:45:31 -0700

    fixed the bot status

--- scripts/main.py
+++ scripts/main.py
@@ -1,6 +1,5 @@
 from keep_alive import keep_alive
 import discord
-from discord.ext.commands import Bot
 from discord.ext import commands
 import json
 import find
@@ -10,18 +9,15 @@ import misc
 
 PREFIX = '$'
 client = discord.Client()
-bot = commands.Bot(command_prefix=PREFIX, description='hi')
 
 with open('scripts/config.json') as f:
   data = json.load(f)
 key = data['key']
 
-@bot.event
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
-    activity = discord.Game(name="$help - utdcoursebot.gg", type=3)
-    await bot.change_presence(status=discord.Status.idle, activity=activity)
+    await client.change_presence(activity=discord.Game(name="$help - utdcoursebot.gg"))
 
 # scan all messages sent
 @client.event
@@ -30,16 +26,16 @@ async def on_message(message):
         return
 
     # if any message starts with $find, get a professor's contact information and details
-    if message.content.startswith('{PREFIX}find'):
+    if message.content.startswith(f'{PREFIX}find'):
         await find.get_professor(message)
     # return a graph of the grades distribution for a class or a professor's section
-    if message.content.startswith('{PREFIX}grades'):
+    if message.content.startswith(f'{PREFIX}grades'):
         await grades.get_grades(message)
     # return the RateMyProfessor ratings for a specified professor
-    if message.content.startswith('{PREFIX}rmp'):
+    if message.content.startswith(f'{PREFIX}rmp'):
         await rmp.get_rating(message)
     # return a list of commands and instructions
-    if message.content.startswith('{PREFIX}help'):
+    if message.content.startswith(f'{PREFIX}help'):
         await misc.get_help(message)
 
 
